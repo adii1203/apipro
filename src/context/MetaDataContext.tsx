93c90a115a505c87e04ca44ee46da562c74e0332
adii1203 <aditya32ft@gmail.com>
2024-04-05 16:07:23 +0530

    Add meta data context provider and wrap main.tsx

---
+++ src/context/MetaDataContext.tsx
@@ -0,0 +1,44 @@
+import { MetaTageTypes } from "@/lib/tpyes";
+import { createContext, useState } from "react";
+
+interface MetaDataContextTypes {
+  url: string;
+  setUrl: (url: string) => void;
+  metaTags: MetaTageTypes;
+  setMetaTags: (metaTags: MetaTageTypes) => void;
+  isLoading: boolean;
+  setIsLoading: (isLoading: boolean) => void;
+}
+
+export const MetaDataContext = createContext<MetaDataContextTypes | undefined>(
+  undefined
+);
+
+export const MetadataContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [url, setUrl] = useState("https://github.com");
+  const [metaTags, setMetaTags] = useState<MetaTageTypes>({
+    title: "",
+    description: "",
+    image: "",
+    url: "",
+    icon: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <MetaDataContext.Provider
+      value={{
+        url,
+        setUrl,
+        metaTags,
+        setMetaTags,
+        isLoading,
+        setIsLoading,
+      }}>
+      {children}
+    </MetaDataContext.Provider>
+  );
+};
