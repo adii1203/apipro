5cc2b18cc77b4a5d4dd6cd307362f921bd556083
adii1203 <aditya32ft@gmail.com>
2024-04-06 13:42:20 +0530

    Update footer in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -142,6 +142,19 @@ function App() {
               </button>
             )}
           </div>
+
+          <div className="text-center mt-6">
+            <p className="text-xs font-Poppins text-foreground">
+              Made with ❤️ by{" "}
+              <a
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://twitter.com/adiidev12" // Add your github link here
+                className="text-blue-500 hover:underline">
+                @adiidev12
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </>
